7526d6a7459a7b5603f9ce51f24c26c3b8452da2
Shawn Song <125272433+sshawnsong@users.noreply.github.com>
2023-05-22 13:17:46 +0900

    second commit

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('StockX-Data-Contest-2019-3 2.csv')
 dateList = df['Order Date'].tolist()
+salePriceList = df['Sale Price'].tolist()
 dateCountDict = {}
+dateCount2017, dateCount2018, dateCount2019 = [], [], []
+salePrice2017, salePrice2018, salePrice2019 = {}, {}, {}
 
 for i in dateList:
   dateIndex = dateList.index(i)
@@ -24,10 +27,6 @@ for i in dateList:
     elif i in dateCountDict:
       dateCountDict[i] += 1
 
-dateCount2017 = []
-dateCount2018 = []
-dateCount2019 = []
-
 for i in list(dateCountDict):
   if int(i) >= 20170000 and int(i) < 20180000:
     dateCount2017.append(dateCountDict[i])
@@ -36,9 +35,24 @@ for i in list(dateCountDict):
   else:
     dateCount2019.append(dateCountDict[i])
 
+for i in range(len(dateList)):
+  if int(dateList[i]) >= 20170000 and int(dateList[i]) < 20180000:
+    salePrice2017[dateList[i]] = int(salePriceList[i].replace('$', '').replace(',', ''))
+  elif int(dateList[i]) >= 20180000 and int(dateList[i]) < 20190000:
+    salePrice2018[dateList[i]] = int(salePriceList[i].replace('$', '').replace(',', ''))
+  else:
+    salePrice2019[dateList[i]] = int(salePriceList[i].replace('$', '').replace(',', ''))
+
 avg2017 = sum(dateCount2017) / len(dateCount2017)
 avg2018 = sum(dateCount2018) / len(dateCount2018)
 avg2019 = sum(dateCount2019) / len(dateCount2019)
 
+avgPrice2017 = sum(salePrice2017.keys()) / len(salePrice2017)
+avgPrice2018 = sum(salePrice2018.keys()) / len(salePrice2018)
+avgPrice2019 = sum(salePrice2019.keys()) / len(salePrice2019)
+
 plt.plot(['2017', '2018', '2019'], [avg2017, avg2018, avg2019])
+plt.show()
+
+plt.plot(['2017', '2018', '2019'], [avgPrice2017, avgPrice2018, avgPrice2019])
 plt.show()
